d7d1f156514f421a859c7f4b28f2f3b830553f9e
LeoLePlug <139858319+LeoLePlug@users.noreply.github.com>
2023-09-06 19:02:17 +0200

    Update leoleplug.php

--- leoleplug.php
+++ leoleplug.php
@@ -1,12 +1,12 @@
 /*
 Plugin Name: LeoLePlug Agency 
 Description: Ce plugin sert à faire fonctionner les sites développé par Leoleplug Agency
-Version: 1.3
+Version: 1.6
 Author: LeoLePlug Agency
 Author URI: https://leoleplug.com/
 */
 
-define('LEOLEPLUG_CURRENT_VERSION', '1.3');
+define('LEOLEPLUG_CURRENT_VERSION', '1.6');
 
 // Inclure le fichier de mise à jour
 require_once(plugin_dir_path(__FILE__) . 'includes/maj-llp.php');
