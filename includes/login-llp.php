a0f8a1ce463c6acbc6dace374c1011ce7b9c41c7
LeoLePlug <139858319+LeoLePlug@users.noreply.github.com>
2023-09-06 16:10:53 +0200

    Update login-llp.php

--- includes/login-llp.php
+++ includes/login-llp.php
@@ -3,7 +3,7 @@
 function leoleplug_custom_login_logo() {
     echo '<style>
         body.login #login h1 a {
-            background-image: url(' . plugin_dir_url(__FILE__) . 'content/logo-llp.png);
+            background-image: url(' . plugin_dir_url(dirname(__FILE__)) . 'content/logo-llp.png);
             background-size: contain;
             width: 100px; // Largeur du logo
             height: 100px; // Hauteur du logo
