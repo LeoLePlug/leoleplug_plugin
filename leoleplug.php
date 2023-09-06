9f41cbeb4ab2cc8d41d1463492180d3a1cb5cfa6
LeoLePlug <139858319+LeoLePlug@users.noreply.github.com>
2023-09-06 15:35:55 +0200

    Add files via upload

---
+++ leoleplug.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: LeoLePlug Agency 
+Description: Ce plugin sert à faire fonctionner les sites développé par Leoleplug Agency
+Version: 1.2
+Author: LeoLePlug Agency
+Author URI: https://leoleplug.com/
+*/
+
+// Inclure le fichier de mise à jour
+require_once(plugin_dir_path(__FILE__) . 'includes/maj-llp.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/support-llp.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/adminpanel-llp.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/login-llp.php');
+
+// Mettre à jour la version actuelle dans les options
+if (!get_option('leoleplug_plugin_version')) {
+    update_option('leoleplug_plugin_version', '1.2');
+}
